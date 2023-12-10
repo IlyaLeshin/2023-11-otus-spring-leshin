c48c4be6546c337155c6588c849e06e86d93f029
IlyaLeshin <ilya139@list.ru>
2023-12-10 14:54:37 +0300

    main part of homework is done

--- hw01-spring-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-spring-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,10 +1,19 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -18,6 +27,35 @@ public class CsvQuestionDao implements QuestionDao {
         // Использовать QuestionReadException
         // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
 
-        return new ArrayList<>();
+        try (InputStream inputStream = getFileFromResource(fileNameProvider.getTestFileName());
+             InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
+
+            CsvToBean<QuestionDto> csvToBean = new CsvToBeanBuilder<QuestionDto>(streamReader)
+                    .withSkipLines(1)
+                    .withSeparator(';')
+                    .withType(QuestionDto.class)
+                    .build();
+            List<Question> questionList = csvToBean
+                    .parse()
+                    .stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+            return questionList;
+
+        } catch (IOException | URISyntaxException e) {
+            throw new QuestionReadException("error reading a file from resources", e);
+        }
+    }
+
+
+    private InputStream getFileFromResource(String fileName) throws URISyntaxException, IOException {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("file not found !" + fileName);
+        } else {
+            return resource.openConnection().getInputStream();
+        }
     }
 }
