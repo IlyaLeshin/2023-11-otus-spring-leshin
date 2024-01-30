17a13c60f72273358c3bb2000bd0804d3eeb69de
IlyaLeshin <ilya139@list.ru>
2024-01-30 08:26:52 +0300

    added tests and fixed checkstyle

---
+++ hw05-spring-jdbc/src/test/java/ru/otus/hw/repositories/JdbcGenreRepositoryTest.java
@@ -0,0 +1,64 @@
+package ru.otus.hw.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import ru.otus.hw.models.Genre;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Репозиторий на основе Jdbc для работы с жанрами ")
+@JdbcTest
+@Import({JdbcGenreRepository.class})
+class JdbcGenreRepositoryTest {
+
+    @Autowired
+    private JdbcGenreRepository repositoryJdbc;
+
+    private List<Genre> dbGenres;
+
+    private Set<Long> dbGenresIds;
+
+    @BeforeEach
+    void setUp() {
+        dbGenres = getDbGenres();
+    }
+
+    @DisplayName("должен загружать список всех жанров по id")
+    @ParameterizedTest
+    @MethodSource({"getDbGenres"})
+    void shouldReturnCorrectGenresListById() {
+        Set<Long> expectedSet = dbGenres.stream().map(Genre::getId).collect(Collectors.toSet());
+        var actualGenres = repositoryJdbc.findAllByIds(expectedSet);
+        var expectedGenres = dbGenres;
+
+        assertThat(actualGenres).containsExactlyElementsOf(expectedGenres);
+        actualGenres.forEach(System.out::println);
+    }
+
+    @DisplayName("должен загружать список всех жанров")
+    @Test
+    void shouldReturnCorrectGenresList() {
+        var actualGenres = repositoryJdbc.findAll();
+        var expectedGenres = dbGenres;
+
+        assertThat(actualGenres).containsExactlyElementsOf(expectedGenres);
+        actualGenres.forEach(System.out::println);
+    }
+
+    private static List<Genre> getDbGenres() {
+        return IntStream.range(1, 7).boxed()
+                .map(id -> new Genre(id, "Genre_" + id))
+                .toList();
+    }
+}
