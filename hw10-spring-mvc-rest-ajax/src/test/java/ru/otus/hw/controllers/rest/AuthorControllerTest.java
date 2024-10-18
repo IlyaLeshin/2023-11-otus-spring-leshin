0a87f574a7e75d561a39405a4f447320d5f01158
IlyaLeshin <ilya139@list.ru>
2024-10-18 03:58:55 +0300

    fixed html

--- hw10-spring-mvc-rest-ajax/src/test/java/ru/otus/hw/controllers/rest/AuthorControllerTest.java
+++ hw10-spring-mvc-rest-ajax/src/test/java/ru/otus/hw/controllers/rest/AuthorControllerTest.java
@@ -8,13 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.otus.hw.dto.AuthorDto;
-import ru.otus.hw.models.Author;
 import ru.otus.hw.services.AuthorService;
 
 import java.util.List;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
