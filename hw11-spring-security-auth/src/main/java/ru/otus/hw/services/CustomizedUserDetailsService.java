a2cff9dd12de42bae20f8edf01fea242a3b57da3
IlyaLeshin <ilya139@list.ru>
2024-10-24 21:14:44 +0300

    fixed CustomizedUserDetailsService

--- hw11-spring-security-auth/src/main/java/ru/otus/hw/services/CustomizedUserDetailsService.java
+++ hw11-spring-security-auth/src/main/java/ru/otus/hw/services/CustomizedUserDetailsService.java
@@ -9,17 +9,19 @@ import ru.otus.hw.dto.UserDto;
 
 @RequiredArgsConstructor
 @Service
-public class CusomizedUserDetailsService implements UserDetailsService {
+public class CustomizedUserDetailsService implements UserDetailsService {
 
     private final UserService userService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserDto userDto = userService.findByUsername(username);
-
-        return org.springframework.security.core.userdetails.User.builder()
-                .username(userDto.getUsername())
-                .password(userDto.getPassword())
-                .build();
+        if (userDto != null) {
+            return org.springframework.security.core.userdetails.User.builder()
+                    .username(userDto.getUsername())
+                    .password(userDto.getPassword())
+                    .build();
+        }
+        throw new UsernameNotFoundException("Username %s not found".formatted(username));
     }
 }
