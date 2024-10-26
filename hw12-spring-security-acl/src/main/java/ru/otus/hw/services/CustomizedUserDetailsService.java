5cf3bde156203b589502c9963e150f19a90ca89b
IlyaLeshin <ilya139@list.ru>
2024-10-26 16:12:38 +0300

    hw12-spring-security-acl (ДЗ №12: Ввести авторизацию на основе URL и/или доменных сущностей) created template

---
+++ hw12-spring-security-acl/src/main/java/ru/otus/hw/services/CustomizedUserDetailsService.java
@@ -0,0 +1,29 @@
+package ru.otus.hw.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.otus.hw.dto.UserDto;
+import ru.otus.hw.exceptions.UserNotFoundException;
+
+@RequiredArgsConstructor
+@Service
+public class CustomizedUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+            UserDto userDto = userService.findByUsername(username);
+            return org.springframework.security.core.userdetails.User.builder()
+                    .username(userDto.getUsername())
+                    .password(userDto.getPassword())
+                    .build();
+        } catch (UserNotFoundException e) {
+            throw new UsernameNotFoundException("Username %s not found".formatted(username), e);
+        }
+    }
+}
